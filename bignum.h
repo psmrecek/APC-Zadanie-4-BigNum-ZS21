0cbe5fca023b1d7b55f0dbd16230dbcb9d9def6c
Peter Smrecek <petersmrecek@gmail.com>
2021-11-27 19:32:47 +0100

    +, -, ordering and unary finished

--- bignum.h
+++ bignum.h
@@ -3,6 +3,7 @@
 // here you can include whatever you want :)
 #include <string>
 #include <stdint.h>
+#include <iostream>
 
 #define SUPPORT_DIVISION 0 // define as 1 when you have implemented the division
 #define SUPPORT_IFSTREAM 0 // define as 1 when you have implemented the input >>
@@ -11,23 +12,160 @@
 class BigNum final
 {
 public:
+    friend std::ostream& operator<<(std::ostream& lhs, const BigNum& rhs);
+    friend bool operator==(const BigNum& lhs, const BigNum& rhs);
+    friend bool operator!=(const BigNum& lhs, const BigNum& rhs);
+    friend bool operator<(const BigNum& lhs, const BigNum& rhs);
+    friend bool operator>(const BigNum& lhs, const BigNum& rhs);
+    friend bool operator<=(const BigNum& lhs, const BigNum& rhs);
+    friend bool operator>=(const BigNum& lhs, const BigNum& rhs);
     // constructors
-    BigNum();
-    BigNum(int64_t n);
-    explicit BigNum(const std::string& str);
+    BigNum() : m_number{ "0" }, m_positive{ true } {}
+    BigNum(int64_t n)
+    {
+        if (n < 0)
+        {
+            m_positive = false;
+            n *= -1;
+        }
+        else
+        {
+            m_positive = true;
+        }
+        m_number = std::to_string(n);
+    }
+    explicit BigNum(const std::string& str)
+    {
+        if (str.length() == 0)
+        {
+            m_number = "0";
+            m_positive = true;
+        }
+        else
+        {
+            bool ignore_first = false;
+            if (str[0] == '-')
+            {
+                m_positive = false;
+                ignore_first = true;
+            }
+            else if (str[0] == '+')
+            {
+                m_positive = true;
+                ignore_first = true;
+            }
+            else
+            {
+                m_positive = true;
+                ignore_first = false;
+            }
+
+            std::string new_str = str;
+            if (ignore_first)
+            {
+                if (str.length() == 1) throw "Unable to construct BigNum";
+                new_str = str.substr(1);
+            }
+
+            for (size_t i = 0; i < new_str.length(); i++)
+            {
+                if (std::isdigit(new_str[i]) == 0) throw "Unable to construct BigNum";
+            }
+            size_t not_zero = new_str.find_first_not_of("0");
+            if (not_zero == std::string::npos)
+            {
+                m_number = "0";
+                m_positive = true;
+            }
+            else
+            {
+                m_number = new_str.substr(not_zero);
+            }
+        }
+    }
 
     // copy
-    BigNum(const BigNum& other);
-    BigNum& operator=(const BigNum& rhs);
+    BigNum(const BigNum& other) = default;
+    BigNum& operator=(const BigNum& rhs)
+    {
+        m_number = rhs.m_number;
+        m_positive = rhs.m_positive;
+        return *this;
+    }
 
     // unary operators
-    const BigNum& operator+() const;
-    BigNum operator-() const;
+    const BigNum& operator+() const
+    {
+        return *this;
+    }
+
+    BigNum operator-() const
+    {
+        BigNum new_bignum = *this;
+        if (new_bignum.m_number == "0")
+        {
+            new_bignum.m_positive = true;
+        }
+        else
+        {
+            new_bignum.m_positive = !new_bignum.m_positive;
+        }
+        return new_bignum;
+    }
 
     // binary arithmetics operators
-    BigNum& operator+=(const BigNum& rhs);
-    BigNum& operator-=(const BigNum& rhs);
-    BigNum& operator*=(const BigNum& rhs);
+    BigNum& operator+=(const BigNum& rhs)
+    {
+        std::string num1{ m_number };
+        std::string num2{ rhs.m_number };
+
+        if (m_positive && rhs.m_positive)
+        {
+            m_number = help_add(num1, num2);
+            m_positive = true;
+        }
+        
+        if (!m_positive && !rhs.m_positive)
+        {
+            m_number = help_add(num1, num2);
+            m_positive = false;
+        }
+
+        if ((m_positive && !rhs.m_positive) || (!m_positive && rhs.m_positive))
+        {
+            m_number = help_sub(num1, num2);
+        }
+
+        return *this;
+    }
+    BigNum& operator-=(const BigNum& rhs)
+    {
+        std::string num1{ m_number };
+        std::string num2{ rhs.m_number };
+
+        if (m_positive && !rhs.m_positive)
+        {
+            m_number = help_add(num1, num2);
+            m_positive = true;
+        }
+
+        if (!m_positive && rhs.m_positive)
+        {
+            m_number = help_add(num1, num2);
+            m_positive = false;
+        }
+        
+        if ((m_positive && rhs.m_positive) || (!m_positive && !rhs.m_positive))
+        {
+            m_number = help_sub(num1, num2);
+        }
+
+        return *this;
+    }
+    BigNum& operator*=(const BigNum& rhs)
+    {
+
+    }
 
 #if SUPPORT_DIVISION == 1
     BigNum& operator/=(const BigNum& rhs); // bonus
@@ -37,11 +175,144 @@ public:
 private:
     // here you can add private data and members, but do not add stuff to 
     // public interface, also you can declare friends here if you want
+    std::string m_number{ "0" };
+    bool m_positive = true;
+
+    std::string help_add(std::string& num1, std::string& num2)
+    {
+        if (num1.length() > num2.length())
+        {
+            std::swap(num1, num2);
+        }
+
+        int64_t len1 = num1.length();
+        int64_t len2 = num2.length();
+        int64_t diff = len2 - len1;
+
+        std::string result{ "" };
+        int64_t carry = 0;
+        char zero = '0';
+
+        for (int64_t i = len1 - 1; i >= 0; i--)
+        {
+            int64_t intermediate = ((num1[i] - zero) + (num2[i + diff] - zero) + carry);
+            result.push_back(intermediate % 10 + zero);
+            carry = intermediate / 10;
+        }
+
+        for (int64_t i = len2 - len1 - 1; i >= 0; i--)
+        {
+            int64_t intermediate = ((num2[i] - zero) + carry);
+            result.push_back(intermediate % 10 + zero);
+            carry = intermediate / 10;
+        }
+
+        if (carry > 0)
+        {
+            int64_t intermediate = carry + '0';
+            result.push_back((char)intermediate);
+        }
+
+        std::reverse(result.begin(), result.end());
+
+        return result;
+    }
+
+    std::string help_sub(std::string& num1, std::string& num2)
+    {
+        // todo if equal return 0
+
+        int64_t len1 = num1.length();
+        int64_t len2 = num2.length();
+        int64_t diff = len2 - len1;
+
+        bool flip = false;
+        if (diff > 0)
+        {
+            flip = true;
+        }
+        else if (diff < 0)
+        {
+            flip = false;
+        }
+        else
+        {
+            for (int64_t i = 0; i < len1; i++)
+            {
+                if (num1[i] < num2[i])
+                {
+                    flip = true;
+                    break;
+                }
+                else if (num1[i] > num2[i])
+                {
+                    flip = false;
+                    break;
+                }
+            }
+        }
+
+        if (flip)
+        {
+            std::swap(num1, num2);
+            m_positive = !m_positive;
+        }
+
+        len1 = num1.length();
+        len2 = num2.length();
+        diff = len2 - len1;
+
+        std::string result{ "" };
+        int64_t carry = 0;
+        char zero = '0';
+
+        for (int64_t i = len2 - 1; i >= 0; i--) {
+            int64_t intermediate = ((num1[i + diff] - zero) - (num2[i] - zero) - carry);
+            if (intermediate < 0)
+            {
+                intermediate = intermediate + 10;
+                carry = 1;
+            }
+            else
+            {
+                carry = 0;
+            }
+            intermediate += zero;
+            result.push_back((char)intermediate);
+        }
+
+        for (int64_t i = len1 - len2 - 1; i >= 0; i--) {
+            if ((num1[i] == zero) && (carry > 0)) {
+                result.push_back('9');
+                continue;
+            }
+            int64_t intermediate = ((num1[i] - zero) - carry);
+            if (i > 0 || intermediate > 0)
+            {
+                intermediate += zero;
+                result.push_back((char)intermediate);
+            }
+
+            carry = 0;
+        }
+
+        reverse(result.begin(), result.end());
+        return result;
+    }
 };
 
-BigNum operator+(BigNum lhs, const BigNum& rhs);
-BigNum operator-(BigNum lhs, const BigNum& rhs);
-BigNum operator*(BigNum lhs, const BigNum& rhs);
+BigNum operator+(BigNum lhs, const BigNum& rhs)
+{
+    return lhs += rhs;
+}
+BigNum operator-(BigNum lhs, const BigNum& rhs)
+{
+    return lhs -= rhs;
+}
+BigNum operator*(BigNum lhs, const BigNum& rhs)
+{
+    return lhs *= rhs;
+}
 
 #if SUPPORT_DIVISION == 1
 BigNum operator/(BigNum lhs, const BigNum& rhs); // bonus
@@ -51,15 +322,97 @@ BigNum operator%(BigNum lhs, const BigNum& rhs); // bonus
 // alternatively you can implement 
 // std::strong_ordering operator<=>(const BigNum& lhs, const BigNum& rhs);
 // idea is, that all comparison should work, it is not important how you do it
-bool operator==(const BigNum& lhs, const BigNum& rhs);
-bool operator!=(const BigNum& lhs, const BigNum& rhs);
-bool operator<(const BigNum& lhs, const BigNum& rhs);
-bool operator>(const BigNum& lhs, const BigNum& rhs);
-bool operator<=(const BigNum& lhs, const BigNum& rhs);
-bool operator>=(const BigNum& lhs, const BigNum& rhs);
+bool operator==(const BigNum& lhs, const BigNum& rhs)
+{
+    return ((lhs.m_number == rhs.m_number) && (lhs.m_positive == rhs.m_positive));
+}
+bool operator!=(const BigNum& lhs, const BigNum& rhs)
+{
+    return !operator==(lhs, rhs);
+}
+bool operator<(const BigNum& lhs, const BigNum& rhs)
+{
+    if (operator==(lhs, rhs))
+    {
+        return false;
+    }
+
+    if (!lhs.m_positive && rhs.m_positive)
+    {
+        return true;
+    }
+
+    if (lhs.m_positive && !rhs.m_positive)
+    {
+        return false;
+    }
+
+    int64_t len1 = lhs.m_number.length();
+    int64_t len2 = rhs.m_number.length();
+    int64_t diff = len2 - len1;
+
+    bool return_value_true = true;
+    bool return_value_false = false;
+
+    if (!lhs.m_positive && !rhs.m_positive)
+    {
+        return_value_true = false;
+        return_value_false = true;
+    }
+
+    if (diff > 0)
+    {
+        return return_value_true;
+    }
+    else if (diff < 0)
+    {
+        return return_value_false;
+    }
+    else
+    {
+        std::string num1{ lhs.m_number };
+        std::string num2{ rhs.m_number };
+        for (int64_t i = 0; i < len1; i++)
+        {
+            if (num1[i] < num2[i])
+            {
+                return return_value_true;
+            }
+            else if (num1[i] > num2[i])
+            {
+                return return_value_false;
+            }
+        }
+    }
+    return return_value_false;
+}
+bool operator>(const BigNum& lhs, const BigNum& rhs)
+{
+    return !operator==(lhs, rhs) && !operator<(lhs, rhs);
+}
+bool operator<=(const BigNum& lhs, const BigNum& rhs)
+{
+    return operator==(lhs, rhs) || operator<(lhs, rhs);
+}
+bool operator>=(const BigNum& lhs, const BigNum& rhs)
+{
+    return operator==(lhs, rhs) || operator>(lhs, rhs);
+}
 
 
-std::ostream& operator<<(std::ostream& lhs, const BigNum& rhs);
+std::ostream& operator<<(std::ostream& lhs, const BigNum& rhs)
+{
+    if (rhs.m_positive)
+    {
+        lhs << rhs.m_number;
+    }
+    else
+    {
+        lhs << "-" << rhs.m_number;
+    }
+    
+    return lhs;
+}
 
 #if SUPPORT_IFSTREAM == 1
 std::istream& operator>>(std::istream& lhs, BigNum& rhs); // bonus
--- main.cpp
+++ main.cpp
@@ -1,9 +1,124 @@
-
+#include "bignum.h"
 #include <iostream>
 
 //int main(int argc, char* argv[])
 int main()
 {
-	std::cout << "Hello world!" << std::endl;
+
+	//try
+	//{
+	//	const std::string c{ "+" };
+	//	BigNum num3(c);
+	//}
+	//catch (const char* e)
+	//{
+	//	std::cout << e << std::endl;
+	//}
+
+	int64_t a = +150;
+	int64_t b = 600;
+
+	const std::string c{ "-0000150" };
+	const std::string d{ "-0000600" };
+
+	BigNum num1 = a;
+	BigNum num2 = b;
+	BigNum num3(c);
+	BigNum num4(d);
+	BigNum num5;
+
+	std::cout << "----------print----------" << std::endl;
+	std::cout << num1 << " (150)" << std::endl;
+	std::cout << num2 << " (600)" << std::endl;
+	std::cout << num3 << " (-150)" << std::endl;
+	std::cout << num4 << " (-600)" << std::endl;
+	std::cout << num5 << " (0)" << std::endl;
+
+	std::cout << "----------add----------" << std::endl;
+	std::cout << num1 + num2 << " (750)" << std::endl;
+	std::cout << num3 + num4 << " (-750)" << std::endl;
+	std::cout << num3 + num2 << " (450)" << std::endl;
+	std::cout << num1 + num4 << " (-450)" << std::endl;
+	std::cout << num2 + num1 << " (750)" << std::endl;
+	std::cout << num4 + num3 << " (-750)" << std::endl;
+	std::cout << num2 + num3 << " (450)" << std::endl;
+	std::cout << num4 + num1 << " (-450)" << std::endl;
+	std::cout << num5 + num5 << " (0)" << std::endl;
+
+	std::cout << "----------sub----------" << std::endl;
+	std::cout << num1 - num2 << " (-450)" << std::endl;
+	std::cout << num3 - num4 << " (450)" << std::endl;
+	std::cout << num3 - num2 << " (-750)" << std::endl;
+	std::cout << num1 - num4 << " (750)" << std::endl;
+	std::cout << num2 - num1 << " (450)" << std::endl;
+	std::cout << num4 - num3 << " (-450)" << std::endl;
+	std::cout << num2 - num3 << " (750)" << std::endl;
+	std::cout << num4 - num1 << " (-750)" << std::endl;
+	std::cout << num1 - num1 << " (0)" << std::endl;
+
+	std::cout << "----------mul----------" << std::endl;
+	std::cout << num1 * num2 << " (90000)" << std::endl;
+	std::cout << num3 * num4 << " (90000)" << std::endl;
+	std::cout << num3 * num2 << " (-90000)" << std::endl;
+	std::cout << num1 * num4 << " (-90000)" << std::endl;
+
+	std::cout << "---------ord ==----------" << std::endl;
+	std::cout << (num1 == num1) << " (1)" << std::endl;
+	std::cout << (num1 == num2) << " (0)" << std::endl;
+	std::cout << (num1 == num3) << " (0)" << std::endl;
+	std::cout << (num1 == num3) << " (0)" << std::endl;
+	std::cout << "---------ord !=----------" << std::endl;
+	std::cout << (num1 != num1) << " (0)" << std::endl;
+	std::cout << (num1 != num2) << " (1)" << std::endl;
+	std::cout << (num1 != num3) << " (1)" << std::endl;
+	std::cout << (num1 != num4) << " (1)" << std::endl;
+	std::cout << "---------ord <----------" << std::endl;
+	std::cout << (num1 < num1) << " (0)" << std::endl;
+	std::cout << (num1 < num2) << " (1)" << std::endl;
+	std::cout << (num1 < num3) << " (0)" << std::endl;
+	std::cout << (num1 < num4) << " (0)" << std::endl;
+	std::cout << "---------ord <=----------" << std::endl;
+	std::cout << (num1 <= num1) << " (1)" << std::endl;
+	std::cout << (num1 <= num2) << " (1)" << std::endl;
+	std::cout << (num1 <= num3) << " (0)" << std::endl;
+	std::cout << (num1 <= num4) << " (0)" << std::endl;
+	std::cout << "---------ord >----------" << std::endl;
+	std::cout << (num1 > num1) << " (0)" << std::endl;
+	std::cout << (num1 > num2) << " (0)" << std::endl;
+	std::cout << (num1 > num3) << " (1)" << std::endl;
+	std::cout << (num1 > num4) << " (1)" << std::endl;
+	std::cout << "---------ord >-----------" << std::endl;
+	std::cout << (num1 >= num1) << " (1)" << std::endl;
+	std::cout << (num1 >= num2) << " (0)" << std::endl;
+	std::cout << (num1 >= num3) << " (1)" << std::endl;
+	std::cout << (num1 >= num4) << " (1)" << std::endl;
+
+	std::cout << "----------unary '+'----------" << std::endl;
+	std::cout << +num1 << " (150)" << std::endl;
+	std::cout << +num2 << " (600)" << std::endl;
+	std::cout << +num3 << " (-150)" << std::endl;
+	std::cout << +num4 << " (-600)" << std::endl;
+	std::cout << +num5 << " (0)" << std::endl;
+
+	std::cout << "----------unary '-'----------" << std::endl;
+	std::cout << -num1 << " (-150)" << std::endl;
+	std::cout << -num2 << " (-600)" << std::endl;
+	std::cout << -num3 << " (150)" << std::endl;
+	std::cout << -num4 << " (600)" << std::endl;
+	std::cout << -num5 << " (0)" << std::endl;
+
+	std::cout << "---------unary '='-----------" << std::endl;
+	num1 = num3;
+	num4 = num2;
+	num3 = BigNum(8);
+	num2 = BigNum(10);
+	std::cout << num1 << " (-150)" << std::endl;
+	std::cout << num2 << " (10)" << std::endl;
+	std::cout << num3 << " (8)" << std::endl;
+	std::cout << num4 << " (600)" << std::endl;
+	std::cout << num5 << " (0)" << std::endl;
+
+
+
 	return 0;
 }
